9ff3c995bd3c8ddd8526210b947fe38908a39f3b
Fabio Neves <fabinhoneves09@gmail.com>
2022-11-12 08:25:43 -0300

    feat(relations): created student-professor relation

--- backend/src/database/models/professor/professor.entity.ts
+++ backend/src/database/models/professor/professor.entity.ts
@@ -1,10 +1,15 @@
-import { Entity, PrimaryColumn } from "typeorm";
+import { Entity, JoinColumn, OneToMany, PrimaryColumn } from "typeorm";
 import { BaseEntity } from "../base/entities/base.entity";
+import { Student } from "../student/student.entity";
 
 @Entity({name: 'professors'})
 export class Professor extends BaseEntity {
     @PrimaryColumn({insert: false, default: () => `PC-${Math.floor(Math.random() * (9999999 - 0) + 0)}`})
     code: string;
+
+    @OneToMany(() => Student, (student) => student.professorCode)
+    @JoinColumn({name: "students_id", referencedColumnName: "id"})
+    studentsId: Student[]
 
     // dungeons
     // feedbacks
--- backend/src/database/models/student/student.entity.ts
+++ backend/src/database/models/student/student.entity.ts
@@ -1,10 +1,14 @@
-import { Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { BaseEntity } from "../base/entities/base.entity";
+import { Professor } from "../professor/professor.entity";
 
 @Entity({name: 'students'})
 export class Student extends BaseEntity {
     @PrimaryGeneratedColumn('uuid')
     id: string;
+
+    @ManyToOne(() => Professor, (prof) => prof.code)
+    professorCode: Professor
 
     // dungeons
     // professor
