07a8ed0cb85e89e62be8fd16d6367120f20d82dc
Fabio Neves <fabinhoneves09@gmail.com>
2022-11-13 19:20:34 -0300

    feat(modules): created professor modules

--- backend/src/modules/professor/professor.module.ts
+++ backend/src/modules/professor/professor.module.ts
@@ -1,8 +1,10 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
-import { ProfessorService } from './professor.service';
-import { ProfessorController } from './professor.controller';
+import { Professor } from '@models/professor/professor.entity';
+import { ProfessorController, ProfessorService } from '@modules/professor';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Professor])],
   controllers: [ProfessorController],
   providers: [ProfessorService]
 })
--- backend/src/modules/professor/professor.service.ts
+++ backend/src/modules/professor/professor.service.ts
@@ -1,17 +1,36 @@
-import { CreateProfessorDto, UpdateProfessorDto } from '@models/professor';
-import { Injectable } from '@nestjs/common';
+import { CreateProfessorDto, Professor, UpdateProfessorDto } from '@models/professor';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProfessorService {
+  constructor(
+    @InjectRepository(Professor)
+    private readonly professorRepository: Repository<Professor>
+  ) { }
+
   async create(createProfessorDto: CreateProfessorDto) {
-    return 'This action adds a new professor';
+    if (await this.professorRepository.findOne({
+      where: {
+        email: createProfessorDto.email
+      }
+    })) throw new ForbiddenException('Duplicated entity')
+    return await this.professorRepository.insert(createProfessorDto)
   }
 
   async findOne(code: string) {
-    return `This action returns a #${code} professor`;
+    return await this.professorRepository.findOne({
+      where: {
+        code
+      },
+      relations: ["dungeons", "students"]
+    })
   }
 
   async update(code: string, updateProfessorDto: UpdateProfessorDto) {
-    return `This action updates a #${code} professor`;
+    const professor = await this.findOne(code)
+    this.professorRepository.merge(professor, updateProfessorDto)
+    return await this.professorRepository.save(professor)
   }
 }
